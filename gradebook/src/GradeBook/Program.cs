c16db3b3e40c61b0ebd233fceb6d8489902dfab0
dstrauss <fmdenver@gmail.com>
2019-05-08 21:36:48 -0400

    initial commit

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades =  new List<double>() {12.7,10.3,6.11,4.1};
+            grades.Add(52.2);
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number; 
+            }
+            Console.WriteLine($"total score {result:N1}");
+            Console.WriteLine($"Average {result/ grades.Count:N3}" );
+            Console.WriteLine("Student " + args[0]);
+
+
+        }
+    }
+}
